8c4ac35491b0470dffc2138e847af49dc849bc5f
12AMprogrammer <61427512+12AMprogrammer@users.noreply.github.com>
2020-07-24 14:39:29 -0500

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Account_Simulator
+{
+    class BankAccount
+    {
+        //Field for balance
+        private decimal _balance;
+        //Constructor 
+        public BankAccount(decimal startingBalance)
+        {
+            _balance = startingBalance;
+        }
+        //Balance property is read-only here
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+        }
+        //Deposit method
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+        }
+        //A withdrawl method.
+        public void Withdraw(decimal amount)
+        {
+            _balance -= amount;
+        }
+    }
+}
